183b80edb8434a162ccbe6a8c29c50c3257e433a
slacker525600 <ecker.chris.r@gmail.com>
2013-10-26 11:37:23 -0500

    moved jsonify to helper

---
+++ jsonify_db_result.py
@@ -0,0 +1,9 @@
+import json
+
+def proc(r)
+  rResult = r.fetch_row()
+  arResults = []
+  while len(rResult) > 0:
+    arResults.append(rResult)
+    rResult = r.fetch_row()
+  return json.dumps(arResults)
